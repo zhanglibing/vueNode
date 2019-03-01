2fedc20f4715e78bec8664bdcd629486a63bfaed
zhanglibing <lb.zhang@izhaohu.com>
2019-03-01 18:00:22 +0800

    ADD

--- src/main.js
+++ src/main.js
@@ -5,9 +5,12 @@ import router from './router';
 import App from './App';
 import store from './store/index.js';
 
+
+
 Vue.config.productionTip = false;
 
 window.vueEvent = new Vue(); // 专门用作单一事件管理而初始化的全局Vue对象
+
 
 /* eslint-disable no-new */
 new Vue({
